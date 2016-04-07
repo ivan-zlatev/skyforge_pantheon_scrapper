94213f7f7b0f6e6962b5c277923e7d27b9f1a7e9
ivan-zlatev <ivanzlatev90@gmail.com>
2016-04-07 14:10:06 +0300

    added delta fix script

---
+++ fix_delta.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+
+#####################################################################
+#                                                                   #
+#  used to fix the age of members so that delta is 1 after          #
+#                                                                   #
+#####################################################################
+
+import time
+import MySQLdb
+from private_data import LoginCredentials # import credentials
+
+def FixMembersAgeSoThatDeltaIsOne(table, requirement):
+	db = MySQLdb.connect(host=LoginCredentials['mysql_host'],
+					user=LoginCredentials['mysql_username'],
+					passwd=LoginCredentials['mysql_password'],
+					db=LoginCredentials['mysql_db'])
+	cursor = db.cursor()
+	sql = "SELECT * FROM pantheon_age ORDER BY member_id ASC"
+	cursor.execute(sql)
+	age_arr = cursor.fetchall()
+	sql = "SELECT * FROM pantheon_log WHERE status = 1 ORDER BY epoch DESC LIMIT 1"
+	cursor.execute(sql)
+	epoch = cursor.fetchone()
+	sql = "SELECT * FROM pantheon WHERE epoch = '" + str(epoch[0]) + "' ORDER BY member_id ASC"
+	cursor.execute(sql)
+	data_arr = cursor.fetchall()
+	curr_time = int(time.time())
+	for age in age_arr:
+		for data in data_arr:
+			if age[0] in data:
+				new_age = curr_time - ((data[table]/requirement)*604800 + 604800 )
+				sql = "UPDATE pantheon_age SET age = '" + str(new_age) + "' WHERE member_id = '" + str(data[1]) + "'"
+				cursor.execute(sql)
+				
+if __name__ == "__main__":
+	FixMembersAgeSoThatDeltaIsOne( table = 4, requirement = 20000 )
