21d62d517dde9a90ecbe53d3f918e5fce594079e
ivan-zlatev <ivanzlatev90@gmail.com>
2016-04-07 13:21:19 +0300

    added age to private data file

--- private_data.py
+++ private_data.py
@@ -15,6 +15,7 @@ LoginCredentials = {
 	'mysql_password' : "<mysql password>",
 	'mysql_db' : "<mysql database>",
 	'mysql_table' : "<mysql table>",
-	'mysql_log' : "<mysql table>-log"
+	'mysql_log' : "<mysql table>-log",
+	'mysql_member_age' : "<mysql table>-age"
 }
 
